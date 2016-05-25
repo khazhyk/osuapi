c5c17dba9d020b5d40b131e5a3125846bba07bc3
Khazhismel Kumykov <khazhy@gmail.com>
2016-05-25 03:38:02 -0400

    Version bump to 0.0.7

--- osuapi/__init__.py
+++ osuapi/__init__.py
@@ -2,7 +2,7 @@ __title__ = "osssss"
 __author__ = "khazhyk"
 __license__ = "MIT"
 __copyright__ = "Copyright khazhyk"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 from .osu import OsuApi
 from .connectors import *
