6286e4c61cc9510f791256e8a2598bbee13cda7f
khazhyk <khazhy@gmail.com>
2021-04-15 07:47:56 +0100

    v0.0.43

--- osuapi/__init__.py
+++ osuapi/__init__.py
@@ -2,7 +2,7 @@ __title__ = "osuapi"
 __author__ = "khazhyk"
 __license__ = "MIT"
 __copyright__ = "Copyright khazhyk"
-__version__ = "0.0.42"
+__version__ = "0.0.43"
 
 from .osu import OsuApi
 from .connectors import *
